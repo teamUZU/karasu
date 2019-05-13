9e6f937d5ee6ac6287754015eea0a7426793bcfe
tetsuyar <cryson0000@gmail.com>
2019-05-14 04:59:59 +0900

    レスポンシブデザイン対応+ABOUT追加

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/new/', views.post_new, name='post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+	path('about/', views.about, name='about'),
 ]
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
