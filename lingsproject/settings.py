07c9498a413699eb88d1814ada6871c0386c50fb
karasu <teamUZU2019@gmail.com>
2019-05-28 22:32:45 +0900

    for merge 20190528.

--- lingsproject/settings.py
+++ lingsproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'recgdhq%o2h1xa10-iq0qrft-h=xem_i$84yq8s^@fr@(*4!lh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
